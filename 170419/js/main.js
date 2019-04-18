e5705abe157f576a9eb735946450a8100e5e7dc9
Your Name <neurona007@gmail.com>
2019-04-17 19:41:28 -0500

    creacion de nuevo direcotio de la clase de el 170419 y el uso de el metodo filter

---
+++ 170419/js/main.js
@@ -0,0 +1,9 @@
+var number_array = [1, 2, 3, 4, 5];
+
+var new_number_array = number_array.map(function(element, index, array){
+  return element * 3;
+})
+
+var even_array = new_number_array.filter(function(element){
+  return element % 2 === 0;
+})
