a3ce5cbde68cc0619cfb15c9f4392e9e1c4b927b
Your Name <neurona007@gmail.com>
2019-04-23 15:24:25 -0500

    Actalizacion de los archivnos main.js donde se crea la funcion my_curp

---
+++ 220419/js/DOM.js
@@ -0,0 +1,32 @@
+
+var divs = [
+    {
+        'height': '100px',
+        'width': '30px',
+        'backgroundColor': 'red'
+    },
+    {
+        'height': '100px',
+        'width': '30px',
+        'backgroundColor': 'peru'
+    },
+    {
+        'height': '100px',
+        'width': '30px',
+        'backgroundColor': 'yellow'
+    },
+    {
+        'height': '100px',
+        'width': '30px',
+        'backgroundColor': 'purple'
+    },
+    {
+        'height': '100px',
+        'width': '30px',
+        'backgroundColor': 'black'
+    },
+]
+
+document.getElementsByTagName('div').style[divs.forEach(function(item){
+    return item;
+})];
---
+++ 220419/js/main.js
@@ -0,0 +1,60 @@
+// Dado un objto sacar la primera parte de el CURP
+
+//Creacion de mi array de objetos
+var persona_array = [{
+    'nombre': 'Juan',
+    'aPaterno': 'Guzman',
+    'aMaterno': 'Linares',
+    'fechaNacimiento': '1996-02-11',
+    'genero': 'hombre'
+},{
+    'nombre': 'Rodolfo',
+    'aPaterno': 'Olmedo',
+    'aMaterno': 'Jimenez',
+    'fechaNacimiento': '1986-11-14',
+    'genero': 'hombre'
+},
+{
+    'nombre': 'Anna',
+    'aPaterno': 'Martinez',
+    'aMaterno': 'Romero',
+    'fechaNacimiento': '1987-04-16',
+    'genero': 'mujer'
+}];
+
+// GULJ960211H
+
+//Funcion constructora
+function Persona(nombre,aPaterno, aMaterno,fechaNacimiento,genero) {
+    this.name = nombre;
+    this.lastName = aPaterno;
+    this.secLastName = aMaterno;
+    this.birthday = fechaNacimiento;
+    this.gen = genero;
+    this.shortName = function(){ //metodo 
+        return (
+            this.lastName.slice(0,2).toUpperCase() + 
+            this.secLastName.charAt(0).toUpperCase() + 
+            this.name.charAt(0).toUpperCase()
+            );
+    };
+    this.getDate = function (){
+        return (
+            this.birthday.slice(2,4) +
+            this.birthday.slice(5,7) +
+            this.birthday.slice(8)
+        );
+     };
+    this.my_curp = function(){
+        return this.shortName() + this.getDate();
+    };
+};
+
+// crea al objeto persona llamando a su funcion constructora
+var persona = new Persona('Rodolfo','Olmedo','Jimenez','1986-11-14','hombre');
+console.log(persona);
+
+//DESPLIEGUE DE CURP DE EL ARREGLO PRSONA
+prsona_array.forEach(function(){
+
+});
