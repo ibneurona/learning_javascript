084b544238003b8594d809f5ee3cccdbed5387ef
Your Name <neurona007@gmail.com>
2019-04-24 20:25:05 -0500

    acceso al DOM por querySelectorAll

--- 240419/js/main.js
+++ 240419/js/main.js
@@ -6,5 +6,12 @@ console.log(document.getElementsByTagName('ul')[0]);
 console.log(document.getElementsByClassName('even'));
 console.log(document.getElementsByTagName('li')[3]);
 
+// Acceder atravez de QuerySelector
+
+console.log(document.querySelectorAll('div>ul'));
+console.log(document.querySelectorAll('div>ul>li.even'));
+console.log(document.querySelectorAll('div>ul>li')[3]);
+
+
 
 
