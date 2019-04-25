d3cdace90baf0b911df58a21e02e3dfab83f24ae
Your Name <neurona007@gmail.com>
2019-04-24 20:02:50 -0500

    Acceso al DOM

---
+++ 240419/js/main.js
@@ -0,0 +1,10 @@
+// document.write('Hello World');
+//document.getElementsByTagName('body')[0].innerText = "Hello World"
+
+console.log(document.getElementsByTagName('div'));
+console.log(document.getElementsByTagName('ul')[0]);
+console.log(document.getElementsByClassName('even'));
+console.log(document.getElementsByTagName('li')[3]);
+
+
+
