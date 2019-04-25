c94050c4da10290503cc17ff9ab92ade5faffe71
Your Name <neurona007@gmail.com>
2019-04-24 21:56:03 -0500

    creacion de nodos y appendar en el DOM

--- 240419/js/main.js
+++ 240419/js/main.js
@@ -12,6 +12,30 @@ console.log(document.querySelectorAll('div>ul'));
 console.log(document.querySelectorAll('div>ul>li.even'));
 console.log(document.querySelectorAll('div>ul>li')[3]);
 
+//
+document.getElementsByTagName('body')[0].style.margin = '0';
+
+for(i = 0; i< document.querySelectorAll('ul').length; i++){
+    document.querySelectorAll('div>ul')[i].style.border = '2px red solid';
+}
+
+// for (var item of document.getElementsByTagName('li')){
+//     item.style.backgroundColor = 'green';
+// }
+
+//pinta los li de los ul
+for(i = 0; i < document.getElementsByTagName('li').length; i++){
+    document.getElementsByTagName('li')[i].style.backgroundColor = 'orange';
+}
 
 
-
+//Creacion de nodos y appendar en el DOM
+var nodeObj = {
+    'tag': 'div',
+    'child':[
+        {'tag':'h2',
+         'text':'hello'
+        }
+    ]
+}
+ 
