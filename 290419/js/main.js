a3de9c17318c34a48d8f8a997b029f011eb83b8b
Your Name <neurona007@gmail.com>
2019-04-29 20:29:09 -0500

    Eventos de el DOM

---
+++ 290419/js/main.js
@@ -0,0 +1,13 @@
+var automovil = {
+    motor: 2.5,
+    puertas: 5,
+    color: 'Negro',
+    traccion: 'Manual',
+    velocidades: 5,
+    modelo: 'Yaris',
+    marca: 'Toyota',
+    year: 2017,
+}
+
+//Los metodos se utilizan mas para construir cosas realizar la accions
+//los Getter and Setter se tilizan mas para modificar propiedades
