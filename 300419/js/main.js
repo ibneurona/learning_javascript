33fb3cc524dcf16dcef0dee08096cf5c9329c1e2
Your Name <neurona007@gmail.com>
2019-05-02 21:14:11 -0500

    Request y creacion de el JSON

--- 300419/js/main.js
+++ 300419/js/main.js
@@ -1,4 +1,38 @@
 // Dada una lista no ordenada con n elementos 
 // 1.- dos clases de CSS 
+// seleccionar el item y deselecionar los otros
 
-var ul = document.querySelector('ul');
+// var elements  = document.getElementsByClassName('item')
+
+// function eventos(element){
+//     var elementList = document.querySelectorAll('.item')
+//     elementList.forEach(function(e){
+//         e.classList.remove('on')
+//     })
+//     element.classList.add('on')
+// }
+
+
+
+// function addEvents() {
+//     var elementList = document.querySelectorAll('.item')
+//     elementList.forEach(function(node) {
+//       node.addEventListener('click', function() {
+//         changes(node, elementList)
+//       })
+//     })
+//   }
+  
+//   function changes(element, elementList) {
+//     elementList.forEach(function(e) {
+//       e.classList.remove('on')
+//     })
+//     element.classList.add('on')
+//   }
+  
+//   window.onload = function() {
+//     addEvents()
+//   }  
+  
+  
+//Tarea addEventlistener de pesos a euros y dolares y 
